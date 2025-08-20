6d23b7bf3984291501c0fe1b823fbc628d6e82a0
LegoGuy32109 <joshevanhale@gmail.com>
2025-08-19 23:24:21 -0400

    nvm back to https

--- routes/index.tsx
+++ routes/index.tsx
@@ -78,8 +78,8 @@ export default function Home({ data }: PageProps<Data>) {
         <a
           class="text-slate-300 rounded-md bg-slate-900/60 font-extralight
          py-1 px-3 w-min whitespace-nowrap"
-          //href={`https://www.bible.com/bible/1713/psa.146.3`}
-          href={`youversion://bible?reference=Psalm+147:3&version_id=1713`}
+          href={`https://www.bible.com/bible/1713/PSA.146.3`}
+          //href={`youversion://bible?reference=Psalm+147:3&version_id=1713`}
         >
           Psalm 147:3
         </a>
