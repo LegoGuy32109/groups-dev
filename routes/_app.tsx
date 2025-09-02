fb2d5680113d54202484bedad9e5bf96ef66ea20
Josh Hale <joshevanhale@gmail.com>
2025-09-01 23:31:11 -0400

    changed title with fancy nvim config

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>e91students</title>
+        <title>E91Students</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
