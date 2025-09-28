7c699b183b4df4c28780a39b3bc444d45b669be0
Josh Hale <joshevanhale@gmail.com>
2025-09-28 14:34:03 -0400

    whoops, needed to fix layout expand

--- routes/(app)/_layout.tsx
+++ routes/(app)/_layout.tsx
@@ -4,11 +4,9 @@ import { define } from "../../utils.ts";
 export default define.page(
   function Layout({ Component, state }) {
     return (
-      <div class="w-dvw h-dvh bg-slate-800">
-        <div class="relative flex flex-col items-center justify-center">
-          <Component />
-          <CornerMenu state={state} />
-        </div>
+      <div class="w-dvw h-dvh bg-slate-800 flex flex-col items-center justify-center">
+        <Component />
+        <CornerMenu state={state} />
       </div>
     );
   },
